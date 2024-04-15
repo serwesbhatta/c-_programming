9296848cdba55e858322fd1213a274fa52986095
Serwes Bhatta <serwes@dzangolab.com>
2024-04-15 01:35:30 -0500

    chore: fix code indentation and spaces

--- valid_parentheses/validParentheses.cpp
+++ valid_parentheses/validParentheses.cpp
@@ -1,44 +1,43 @@
 #include <iostream>
-#include <string>
 #include <stack>
+#include <string>
 
 using namespace std;
 
 class Solution {
 private:
-    char temp;
-    int i;
-    stack<char> stack;
+  char temp;
+  int i;
+  stack<char> stack;
 
 public:
-    bool validParentheses(string s) {
-        if (s.size() <= 1) {
-            return false;
-        }
-
-        if (s.size() % 2 == 1) {
-            return false;
-        }
-
-        for (int i =0; i < s.size(); i++) {
-            if(stack.empty()) {
-                stack.push(s[i]);
-            }
-            else {
-                if ((stack.top() == '(' && s[i] == ')') || (stack.top() == '{' && s[i] == '}') || (stack.top() == '[' && s[i] == ']')) {
-                    stack.pop();
-                }
-                else {
-                    stack.push(s[i]);
-                }
-            }
-        }
-
-        if(stack.empty()) {
-            return true;
-        } 
-        else {
-            return false;
-        }
+  bool validParentheses(string s) {
+    if (s.size() <= 1) {
+      return false;
     }
+
+    if (s.size() % 2 == 1) {
+      return false;
+    }
+
+    for (int i = 0; i < s.size(); i++) {
+      if (stack.empty()) {
+        stack.push(s[i]);
+      } else {
+        if ((stack.top() == '(' && s[i] == ')') ||
+            (stack.top() == '{' && s[i] == '}') ||
+            (stack.top() == '[' && s[i] == ']')) {
+          stack.pop();
+        } else {
+          stack.push(s[i]);
+        }
+      }
+    }
+
+    if (stack.empty()) {
+      return true;
+    } else {
+      return false;
+    }
+  }
 };
