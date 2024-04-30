b96c4429a7f3dc020b8052132f6c7f5edc3556ea
Serwes Bhatta <serwes@dzangolab.com>
2024-04-30 17:32:28 -0500

    feat: create a main function to implement search insert position function

--- search_insert_position/searchInsertPosition.cpp
+++ search_insert_position/searchInsertPosition.cpp
@@ -45,4 +45,63 @@ public:
       return binarySearch(nums, mid + 1, right, target);
     }
   }
+
+  void printResult(vector<int> &nums, int target, int index) {
+    if (targetFound == true) {
+      cout << "The target " << target << " is found in given {";
+      for (int i = 0; i < nums.size(); ++i) {
+        cout << nums[i];
+        if (i < nums.size() - 1) {
+          cout << ", ";
+        }
+      }
+      cout << "} in the index " << index << "." << endl;
+    } else {
+      cout << "The target " << target << " could not be found in given {";
+      for (int i = 0; i < nums.size(); ++i) {
+        cout << nums[i];
+        if (i < nums.size() - 1) {
+          cout << ", ";
+        }
+      }
+      cout << "} so if the target was to insert in an ascending array it would "
+              "be at index "
+           << index << "." << endl;
+    }
+  }
 };
+
+int main() {
+  Solution S1, S2, S3, S4, S5, S6;
+
+  vector<int> input1234 = {1, 3, 5, 6};
+  vector<int> input5 = {1, 3};
+  vector<int> input6 = {1, 3, 5};
+
+  int t1, t2, t3, t4, t5, t6;
+  int index;
+  t1 = 5;
+  t2 = 2;
+  t3 = 7;
+  t4 = 0;
+  t5 = 2;
+  t6 = 4;
+
+  index = S1.searchInsert(input1234, t1);
+  S1.printResult(input1234, t1, index);
+
+  index = S2.searchInsert(input1234, t2);
+  S2.printResult(input1234, t2, index);
+
+  index = S3.searchInsert(input1234, t3);
+  S3.printResult(input1234, t3, index);
+
+  index = S4.searchInsert(input1234, t4);
+  S4.printResult(input1234, t4, index);
+
+  index = S5.searchInsert(input5, t5);
+  S5.printResult(input5, t5, index);
+
+  index = S6.searchInsert(input6, t6);
+  S6.printResult(input6, t6, index);
+}
