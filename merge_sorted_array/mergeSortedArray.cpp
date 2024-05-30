95adf3b341f05ff8e21c1eda4cc116950bde1862
Serwes Bhatta <serwes@dzangolab.com>
2024-05-29 20:25:07 -0500

    feat: create a main function to implement the merge sorted array function

--- merge_sorted_array/mergeSortedArray.cpp
+++ merge_sorted_array/mergeSortedArray.cpp
@@ -27,4 +27,41 @@ public:
       nums1[k--] = nums2[j--];
     }
   }
+
+  void printArray(vector<int> nums) {
+    for (i = 0; i < nums.size(); i++) {
+      cout << nums[i] << "\t";
+    }
+    cout << endl;
+  }
 };
+
+int main() {
+  Solution S1, S2, S3;
+
+  vector<int> S1nums1, S1nums2, S2nums1, S2nums2, S3nums1, S3nums2;
+  int S1m, S1n, S2m, S2n, S3m, S3n;
+
+  S1nums1 = {1, 2, 3, 0, 0, 0};
+  S1m = 3;
+  S1nums2 = {2, 5, 6};
+  S1n = 3;
+
+  S2nums1 = {1};
+  S2m = 1;
+  S2nums2 = {};
+  S2n = 0;
+
+  S3nums1 = {0};
+  S3m = 0;
+  S3nums2 = {1};
+  S3n = 1;
+
+  S1.merge(S1nums1, S1m, S1nums2, S1n);
+  S2.merge(S2nums1, S2m, S2nums2, S2n);
+  S3.merge(S3nums1, S3m, S3nums2, S3n);
+
+  S1.printArray(S1nums1);
+  S2.printArray(S2nums1);
+  S3.printArray(S3nums1);
+}
